505df379e524a924cf879f9b7cac084d9fdd8042
andresllorens <llorensandres08@gmail.com>
2026-03-17 21:29:06 -0300

    actualizacion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://tecnolobbyx.com',
-  base: '/blog', // Crucial: Esto ajusta todas las rutas internas
-  output: 'static' // Asegura que se generen archivos HTML puros
+
+  // Crucial: Esto ajusta todas las rutas internas
+  base: '/blog',
+
+  // Asegura que se generen archivos HTML puros
+  output: 'static',
+
+  integrations: [sitemap()]
 });
