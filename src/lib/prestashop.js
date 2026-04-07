2015052cbedba8c16fb2aaac1bfce1da32b688a4
andresllorens <llorensandres08@gmail.com>
2026-04-07 16:20:44 -0300

    cambios sugeridos por lucho

--- src/lib/prestashop.js
+++ src/lib/prestashop.js
@@ -26,3 +26,21 @@ export async function getCategoryById(id) {
     return null;
   }
 }
+
+export async function getProductFeatureValue(id) {
+    // Añadimos el /id antes de los parámetros de consulta
+    const response = await fetch(`${BASE_URL}/product_feature_values/${id}?output_format=JSON&ws_key=${API_KEY}`);
+    const data = await response.json();
+    
+    // Accedemos a .product_feature_value y luego a .value
+    return data.product_feature_value.value; 
+}
+
+export async function getProductFeature(id) {
+    // Añadimos el /id antes de los parámetros de consulta
+    const response = await fetch(`${BASE_URL}/product_features/${id}?output_format=JSON&ws_key=${API_KEY}`);
+    const data = await response.json();
+    
+    // Devolvemos el objeto completo (que contiene id, position y name)
+    return data.product_feature;
+}
