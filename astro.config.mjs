ef87fc03da597c4104ee6e2e89d2d3fbb8f315f2
andresllorens <llorensandres08@gmail.com>
2026-04-16 11:29:31 -0300

    correcion rutas

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,17 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://tecnolobbyx.com',
+  site: 'https://tecnolobbyx.com/tienda',
 
   // Crucial: Esto ajusta todas las rutas internas
-  base: '/venta',
+  base: '/',
 
   // Asegura que se generen archivos HTML puros
   output: 'static',
 
   image: {
     // Reemplaza 'tu-prestasop.com' con el dominio real de tu backend
-    domains: ['https://tecnolobbyx.com'], 
+    domains: ['https://tecnolobbyx.com/tienda'], 
   },
 
   integrations: [sitemap()]
